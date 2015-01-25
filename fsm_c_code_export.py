ba08e53919603444cf55b7acb1886ee4f6f08532
Schmelzer <ulf.schmelzer@stud.hs-esslingen.de>
2015-01-25 11:45:09 +0100

    Update fsm_c_code_export.py
    corrected email address

--- fsm_c_code_export.py
+++ fsm_c_code_export.py
@@ -4,7 +4,7 @@
 # and Unai Estébanez Sevilla
 # ----------------------------------------------------------------------------
 # "THE BEER-WARE LICENSE" (Revision 42):
-# <ulf.schmlzr@schmlzr.de> wrote this file. As long as you retain this notice you
+# <ulf.schmelzer@schmlzr.de> wrote this file. As long as you retain this notice you
 # can do whatever you want with this stuff. If we meet some day, and you think
 # this stuff is worth it, you can buy me a beer in return Ulf Schmelzer
 # ----------------------------------------------------------------------------
